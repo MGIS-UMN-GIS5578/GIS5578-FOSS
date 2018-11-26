99ddd313942344118d4685eaedd87dfa529d4057
David Haynes <haynesd2@gmail.com>
2018-11-26 17:26:46 -0600

    fixed

--- automatedImageryDownload.py
+++ automatedImageryDownload.py
@@ -2,22 +2,22 @@
 import urllib
 
 #Input Parameters here
-fileDir = r'/home/david/data/GIS5578/maryland_imagery'
+fileDir = r"e:\maryland_imagery"
 webaddress = r'http://commondatastorage.googleapis.com/earthenginepartners-hansen/GFC2013/treecover2000.txt'
 
 
 #Use urllib to open the url and read it as text string
-webpg = urllib.urlopen(webaddress)
+webpg = urllib.request.urlopen(webaddress)
 data = webpg.read()
 
 #Split the text string on the new line character
-tiles = data.split('\n')
+tiles = data.split(b'\n')
 
 
 for c, tile in enumerate(tiles):
     imagename = "tile_%s" % (c)
-    fileName = r"%s\%s" % (fileDir, imagename)
+    fileName = r"%s\%s.tif" % (fileDir, imagename)
     print(tile,fileName)
-    urllib.urlretrieve(tile, fileName)
+    urllib.request.urlretrieve(tile.decode("utf-8"), fileName)
     #Remove this after it works
     break
